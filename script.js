03d77573d6efe8e5fa64857907f12ac34df99737
Niranjan D <adailyvitalitydose@gmail.com>
2026-04-18 21:51:12 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Fade-in animation logic
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) entry.target.classList.add('on');
-  });
-}, { threshold: 0.1 });
-
-document.querySelectorAll('.rev').forEach(el => observer.observe(el));
